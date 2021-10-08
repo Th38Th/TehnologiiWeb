d088d84914b7cec214559f96fa1c5659d8fc8067
Th38Th <octavian.chiliment@gmail.com>
2021-10-08 09:49:03 +0000

    try2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HELLOW");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(8080);
